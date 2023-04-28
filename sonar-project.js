1fe81edc5dbceb1d2bbf16c65976a496da3f3297
iqis2much <126314295+iqis2much@users.noreply.github.com>
2023-04-27 22:34:54 -0500

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://172.31.26.202:9000/',
+  serverUrl: 'http://23.22.185.155:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
